f43e87a76165d1b9ab704bdec2804f83bad0a9a3
Su nombre <hector.pizher.98@gmail.com>
2020-11-08 14:58:52 -0600

    Se suprimio validacion de campos hasta solucionar error de cierre, por lo que manda a menu sin validacion

--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
     Button btn;
     Button btn2;
-    //EditText user=(EditText)findViewById(R.id.editTextTextEmailAddress);
-    //EditText pass=(EditText)findViewById(R.id.editTextTextPassword);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +22,12 @@ public class MainActivity extends AppCompatActivity {
         btn=findViewById(R.id.button2);
         btn2=findViewById(R.id.button);
 
-        //String user2=user.getText().toString();
-        //String pass2=pass.getText().toString();
-
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //if (user2=="admin" && pass2=="admin"){
                     Intent b= new Intent(MainActivity.this,ActivityMenu.class);
                     startActivity(b);
-                //}else{
-                    //Toast.makeText(getBaseContext(),"Usuario no valido",Toast.LENGTH_SHORT).show();
-                //}
             }
         });
 
