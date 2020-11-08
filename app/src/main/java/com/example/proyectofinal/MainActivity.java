576597d5ba4eb27ada1baeb8343e3d033c169da4
Su nombre <hector.pizher.98@gmail.com>
2020-11-08 15:55:52 -0600

    Se agrego toast de bienvenida (Continua error de validacion, cierra la app)

--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -14,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     Button btn;
     Button btn2;
 
+    EditText a1;
+    EditText a2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,13 +24,20 @@ public class MainActivity extends AppCompatActivity {
 
         btn=findViewById(R.id.button2);
         btn2=findViewById(R.id.button);
+        a1=findViewById(R.id.editTextTextEmailAddress);
+        a2=findViewById(R.id.editTextTextPassword);
 
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //if (a1.getText().toString().length()==0){
+                    Toast toast = Toast.makeText(getBaseContext(), "Bienvenido: ",Toast.LENGTH_SHORT);
+                    toast.show();
+                //}else{
                     Intent b= new Intent(MainActivity.this,ActivityMenu.class);
                     startActivity(b);
+                //}
             }
         });
 
