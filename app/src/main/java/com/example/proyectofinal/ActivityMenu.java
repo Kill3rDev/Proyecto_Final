4afb768a6752b1c7dde8141d5b9d99984c19e8a9
MarcoLG96 <mlievanogarcia@gmail.com>
2020-11-08 22:57:39 -0600

    Marco liévano comentando

--- app/src/main/java/com/example/proyectofinal/ActivityMenu.java
+++ app/src/main/java/com/example/proyectofinal/ActivityMenu.java
@@ -10,5 +10,6 @@ public class ActivityMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        ///Hola Mundo Soy Marco Liévano
     }
 }
