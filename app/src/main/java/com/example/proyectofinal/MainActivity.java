5c639c5747c2ec2405330a1265faddf70b01f8a7
MarcoLG96 <mlievanogarcia@gmail.com>
2020-11-09 16:58:54 -0600

    Agregando elementos a la interfaz de CrearNota

--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -11,11 +11,12 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Cambiando el nombre de las variableas para indetificarlas mejor
     Button btnIniciarSesion;
     Button btnRegistro;
 
-    // EditText a1; SOSPECHA DE ERRROR DE CIERRE
-    // EditText a2; SOSPECHA DE ERRROR DE CIERRE
+    EditText a1;
+    EditText a2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +25,19 @@ public class MainActivity extends AppCompatActivity {
 
         btnIniciarSesion=findViewById(R.id.btnIniciarSesion);
         btnRegistro=findViewById(R.id.btnRegistro);
-        //a1=findViewById(R.id.editTextTextEmailAddress); SOSPECHA DE ERRROR DE CIERRE
-       // a2=findViewById(R.id.editTextTextPassword); SOSPECHA DE ERRROR DE CIERRE
+        a1=findViewById(R.id.editTextTextEmailAddress);
+        a2=findViewById(R.id.editTextTextPassword);
 
 
-       btnIniciarSesion.setOnClickListener(view -> {
-            //if (a1.getText().toString().length()==0){ SOSPECHA DE ERRROR DE CIERRE
+       //btnIniciarSesion.setOnClickListener(view -> {
+            //if (a1.getText().toString().length()==0){
                Toast toast = Toast.makeText(getBaseContext(), "Bienvenido: ",Toast.LENGTH_SHORT);
-               toast.show();
-            //}else{ SOSPECHA DE ERRROR DE CIERRE
-                Intent b = new Intent(MainActivity.this,ActivityMenu.class);
-                startActivity(b);
+               // toast.show();
+            //}else{
+                //Intent b = new Intent(MainActivity.this,ActivityMenu.class);
+                //startActivity(b);
             //}
-       });
+      //  });
 
         btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
