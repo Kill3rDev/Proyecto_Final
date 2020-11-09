9d4c2c95f22c65c35ed0b7cea223fba56d2272f4
Su nombre <hector.pizher.98@gmail.com>
2020-11-09 10:13:58 -0600

    Se corrigio error de cierre en iniciar sesion y se marcaron lineas de posible error al validar campos vacios

--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -11,12 +11,11 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Cambiando el nombre de las variableas para indetificarlas mejor
     Button btnIniciarSesion;
     Button btnRegistro;
 
-    EditText a1;
-    EditText a2;
+    // EditText a1; SOSPECHA DE ERRROR DE CIERRE
+    // EditText a2; SOSPECHA DE ERRROR DE CIERRE
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,19 +24,19 @@ public class MainActivity extends AppCompatActivity {
 
         btnIniciarSesion=findViewById(R.id.btnIniciarSesion);
         btnRegistro=findViewById(R.id.btnRegistro);
-        a1=findViewById(R.id.editTextTextEmailAddress);
-        a2=findViewById(R.id.editTextTextPassword);
+        //a1=findViewById(R.id.editTextTextEmailAddress); SOSPECHA DE ERRROR DE CIERRE
+       // a2=findViewById(R.id.editTextTextPassword); SOSPECHA DE ERRROR DE CIERRE
 
 
-       //btnIniciarSesion.setOnClickListener(view -> {
-            //if (a1.getText().toString().length()==0){
+       btnIniciarSesion.setOnClickListener(view -> {
+            //if (a1.getText().toString().length()==0){ SOSPECHA DE ERRROR DE CIERRE
                Toast toast = Toast.makeText(getBaseContext(), "Bienvenido: ",Toast.LENGTH_SHORT);
-               // toast.show();
-            //}else{
-                //Intent b = new Intent(MainActivity.this,ActivityMenu.class);
-                //startActivity(b);
+               toast.show();
+            //}else{ SOSPECHA DE ERRROR DE CIERRE
+                Intent b = new Intent(MainActivity.this,ActivityMenu.class);
+                startActivity(b);
             //}
-      //  });
+       });
 
         btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
